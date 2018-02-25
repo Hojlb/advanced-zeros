c777d65a0c23c9739aef746e3fc6b13b74c1026a
Maksim Palonski <mchp010nsky@gmail.com>
2018-02-25 22:46:58 +0300

    it is not full solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let count = 1;
+    let result;
+
+    let recidue = number;
+    let numberByBase = [];
+
+    function divideN (rec, bs) {
+        numberByBase.push( rec % bs );
+        return (rec - rec % bs) / bs;
+    }
+
+    while (recidue > 0)
+    {
+        recidue = divideN( recidue, base);
+    }
+
+    numberByBase.push( recidue );
+    result = +numberByBase.reverse().join('');
+    //console.log(result);
+
+    let num = result;
+    let divider = 5;
+
+    while ( result >= base )
+    {
+        result = (num - num % divider) / divider;
+        divider = divider * 5;
+        count += result;
+    }
+
+    if( result < 5 && result >= 2 ){
+        count--;
+    }
+
+    //console.log( count );
+    return( count );
 }
